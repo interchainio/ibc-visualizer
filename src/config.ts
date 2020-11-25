c6a0df0b571ce1cac7389ed17cd6fda847a805bd
Simon Warta <simon@warta.it>
2020-11-25 16:13:35 +0100

    Change coralnet -> musselnet

--- src/config.ts
+++ src/config.ts
@@ -9,11 +9,11 @@ const local: AppConfig = {
   rpcUrl: "http://localhost:26658",
 };
 
-const coralnet: AppConfig = {
-  rpcUrl: "https://rpc.coralnet.cosmwasm.com",
+const musselnet: AppConfig = {
+  rpcUrl: "https://rpc.musselnet.cosmwasm.com",
 };
 
-const configs: NetworkConfigs = { local, coralnet };
+const configs: NetworkConfigs = { local, musselnet };
 
 function getAppConfig(): AppConfig {
   const network = process.env.REACT_APP_NETWORK;
