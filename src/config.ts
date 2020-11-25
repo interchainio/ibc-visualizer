b878bc04043d4e8a06d6210a070fd15583a0f4f5
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-11-25 14:42:49 +0100

    Improve app config

--- src/config.ts
+++ src/config.ts
@@ -13,8 +13,21 @@ const coralnet: AppConfig = {
   rpcUrl: "https://rpc.coralnet.cosmwasm.com",
 };
 
-const networks: NetworkConfigs = {
+const configs: NetworkConfigs = {
+  ["local"]: local,
   ["coralnet"]: coralnet,
 };
 
-export const config = process.env.REACT_APP_NETWORK ? networks[process.env.REACT_APP_NETWORK] : local;
+function getAppConfig(): AppConfig {
+  const network = process.env.REACT_APP_NETWORK;
+  if (!network) return local;
+
+  const config = configs[network];
+  if (!config) {
+    throw new Error(`No configuration found for network ${network}`);
+  }
+
+  return config;
+}
+
+export const config = getAppConfig();
