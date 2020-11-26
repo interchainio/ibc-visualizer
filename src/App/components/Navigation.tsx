0ac783b4f898d75cca1ee0bc007aeacd5b59f8e9
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-11-26 10:03:34 +0100

    Remove Channels component

--- src/App/components/Navigation.tsx
+++ src/App/components/Navigation.tsx
@@ -6,11 +6,8 @@ import { style } from "../style";
 export function Navigation(): JSX.Element {
   return (
     <nav className="my-5">
-      <Link to="/connections" className={`${style.button} ml-0`}>
-        Connections
-      </Link>
-      <Link to="/channels" className={style.button}>
-        Channels
+      <Link to="/" className={`${style.button} ml-0`}>
+        Home
       </Link>
     </nav>
   );
