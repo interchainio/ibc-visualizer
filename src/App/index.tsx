88ba688ddb94ecd400d87afaa01c88458a7c601c
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-11-27 09:52:30 +0100

    Use new route scheme with merged portId channelId

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 
 import { ClientProvider } from "../contexts/ClientContext";
-import { pathAcknowledgements, pathChannels, pathCommitments, pathConnections, pathSequences } from "./paths";
+import { pathAcknowledgements, pathChannels, pathCommitments, pathConnections } from "./paths";
 import { Acknowledgement } from "./routes/Acknowledgement";
 import { Channel } from "./routes/Channel";
 import { Commitment } from "./routes/Commitment";
 import { Connection } from "./routes/Connection";
 import { Connections } from "./routes/Connections";
+
+// portIdChannelId is a joined string of portId and channelId, with a ":" separator:
+// <portId>:<channelId>
+export const portIdChannelIdSeparator = ":";
+const paramChannel = `${pathChannels}/:portIdChannelId`;
+
+const paramConnection = `${pathConnections}/:connectionId`;
+const paramCommitment = `${pathCommitments}/:sequence`;
+const paramAcknowledgement = `${pathAcknowledgements}/:sequence`;
 
 export function App(): JSX.Element {
   return (
@@ -15,16 +24,12 @@ export function App(): JSX.Element {
       <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path={pathConnections} component={Connections} />
-          <Route exact path={`${pathConnections}/:connectionId`} component={Connection} />
-          <Route exact path={`${pathConnections}/:channelId${pathChannels}/:portId`} component={Channel} />
+          <Route exact path={paramConnection} component={Connection} />
+          <Route exact path={`${paramConnection}${paramChannel}`} component={Channel} />
+          <Route exact path={`${paramConnection}${paramChannel}${paramCommitment}`} component={Commitment} />
           <Route
             exact
-            path={`${pathConnections}/:channelId${pathCommitments}/:portId${pathSequences}/:sequence`}
-            component={Commitment}
-          />
-          <Route
-            exact
-            path={`${pathConnections}/:channelId${pathAcknowledgements}/:portId${pathSequences}/:sequence`}
+            path={`${paramConnection}${paramChannel}${paramAcknowledgement}`}
             component={Acknowledgement}
           />
           <Route component={() => <Redirect to={pathConnections} />} />
--- src/App/paths.ts
+++ src/App/paths.ts
@@ -2,4 +2,3 @@ export const pathConnections = "/connections";
 export const pathChannels = "/channels";
 export const pathCommitments = "/commitments";
 export const pathAcknowledgements = "/acknowledgements";
-export const pathSequences = "/sequences";
